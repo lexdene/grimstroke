7dfc1d59de92705d77b4a804ba3b32e372ea28b6
Elephant Liu <lexdene@gmail.com>
2022-07-23 09:59:08 +0800

    support entry

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grimstroke',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(exclude=['tests*']),
     entry_points="""
     [console_scripts]
